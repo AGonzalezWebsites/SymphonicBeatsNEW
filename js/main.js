45ea0f4eb93b17fdb0c9795019e6192821c9897f
AGonzalezWebsites <Agonzalez6259@yahoo.com>
2020-06-23 03:46:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,13 +36,6 @@ init();
 
 
 
-
-
-
-
-
-
-
 /*NEW SCRIPT*/     
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
